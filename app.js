6d9d22f4f38a23054c82ee7e3de964a138a1c988
Hossain <arafat.kd99@gmail.com>
2023-06-25 18:16:24 +0100

    Implement event listener to menu items to hide menu

--- app.js
+++ app.js
@@ -6,23 +6,27 @@
 let toggleButton = document.querySelector(".toggle-button");
 let xIcon = document.querySelector(".x-icon");
 let mainMenu = document.querySelector(".menu");
+let menuItem = document.querySelectorAll(".menu-item");
 
 toggleButton.addEventListener('click', show);
 xIcon.addEventListener('click', hide);
+menuItem.forEach(item => item.addEventListener('click', menuItemClick));
+
 
 function show () {
   mainMenu.style.display = 'flex';
   mainMenu.style.color = '#fff';
   mainMenu.style.backgroundColor = '#f7f7f9';
-
-// mainMenu.style.top = '0';
   toggleButton.style.display =  'none';
   xIcon.style.display = 'flex';
-  xIcon.style.top = '0';
-  xIcon.style.left = '0';
 }
 function hide () {
   mainMenu.style.display = 'none';
   xIcon.style.display = 'none';
   toggleButton.style.display = 'flex';
+}
+
+function menuItemClick () {
+  mainMenu.style.display = 'none';
+  xIcon.style.display = 'none';
 }
