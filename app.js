dbb7a4df8c1e6fa85ddfc73ad5bf855f83ad3daf
Hossain <arafat.kd99@gmail.com>
2023-06-25 17:21:23 +0100

    Impliment event listener on humbarger menu

--- app.js
+++ app.js
@@ -5,11 +5,24 @@
 // .x-icon
 let toggleButton = document.querySelector(".toggle-button");
 let xIcon = document.querySelector(".x-icon");
-let menu = document.querySelector(".menu");
+let mainMenu = document.querySelector(".menu");
 
-toggleButton.addEventListener.apply('click', hidden);
-xIcon.addEventListener.apply('click', shown);
+toggleButton.addEventListener('click', show);
+xIcon.addEventListener('click', hide);
 
-function hidden () {
-  menu.computedStyleMap.display = flex;
+function show () {
+  mainMenu.style.display = 'flex';
+  mainMenu.style.color = '#fff';
+  mainMenu.style.backgroundColor = '#f7f7f9';
+
+// mainMenu.style.top = '0';
+  toggleButton.style.display =  'none';
+  xIcon.style.display = 'flex';
+  xIcon.style.top = '0';
+  xIcon.style.left = '0';
+}
+function hide () {
+  mainMenu.style.display = 'none';
+  xIcon.style.display = 'none';
+  toggleButton.style.display = 'flex';
 }
