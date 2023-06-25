82a892ac4b8c462e36c2e04a0160ea7091b985dd
Hossain <arafat.kd99@gmail.com>
2023-06-25 13:26:26 +0100

    Add veriables in js file

---
+++ app.js
@@ -0,0 +1,15 @@
+// .fa-solid
+// .fa-bars
+// .toggle-button
+// .menu
+// .x-icon
+let toggleButton = document.querySelector(".toggle-button");
+let xIcon = document.querySelector(".x-icon");
+let menu = document.querySelector(".menu");
+
+toggleButton.addEventListener.apply('click', hidden);
+xIcon.addEventListener.apply('click', shown);
+
+function hidden () {
+  menu.computedStyleMap.display = flex;
+}
