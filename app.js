52b6f6bcc71d95df5b75e707a44f53efd648449a
Hossain <arafat.kd99@gmail.com>
2023-06-26 16:40:20 +0100

    Add eslint & correct linters issues

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-let toggleButton = document.querySelector(".toggle-button");
-let xIcon = document.querySelector(".x-icon");
-let mainMenu = document.querySelector(".menu");
-let menuItem = document.querySelectorAll(".menu-item");
-let navbar = document.querySelector('.navbar');
-let body = document.querySelector('body');
-let logo = document.querySelector('.logo');
+const toggleButton = document.querySelector('.toggle-button')
+const xIcon = document.querySelector('.x-icon')
+const mainMenu = document.querySelector('.menu')
+const menuItem = document.querySelectorAll('.menu-item')
+const navbar = document.querySelector('.navbar')
+// const body = document.querySelector('body')
+const logo = document.querySelector('.logo')
 
-toggleButton.addEventListener('click', show);
-xIcon.addEventListener('click', hide);
-menuItem.forEach(item => item.addEventListener('click', menuItemClick));
-
+toggleButton.addEventListener('click', show)
+xIcon.addEventListener('click', hide)
+menuItem.forEach(item => item.addEventListener('click', menuItemClick))
 
 function show () {
-  mainMenu.style.display = 'flex';
-  mainMenu.style.color = '#fff';
-  mainMenu.style.opacity = '.93';
-  toggleButton.style.display =  'none';
-  xIcon.style.display = 'flex';
-  xIcon.style.right = '0';
-  navbar.style.display = 'flex';
-  navbar.style.flexDirection = 'column';
+  mainMenu.style.display = 'flex'
+  mainMenu.style.color = '#fff'
+  mainMenu.style.opacity = '.93'
+  toggleButton.style.display = 'none'
+  xIcon.style.display = 'flex'
+  xIcon.style.right = '0'
+  navbar.style.display = 'flex'
+  navbar.style.flexDirection = 'column'
   logo.style.alignSelf = 'flex-start'
-  logo.style.paddingTop = '9%';
+  logo.style.paddingTop = '9%'
 }
 
 function hide () {
-  mainMenu.style.display = 'none';
-  xIcon.style.display = 'none';
-  toggleButton.style.display = 'flex';
-  toggleButton.style.justifyContent = 'flex-end';
-  navbar.style.flexDirection = 'row';
-  logo.style.paddingTop = '9%';
+  mainMenu.style.display = 'none'
+  xIcon.style.display = 'none'
+  toggleButton.style.display = 'flex'
+  toggleButton.style.justifyContent = 'flex-end'
+  navbar.style.flexDirection = 'row'
+  logo.style.paddingTop = '9%'
 }
 
 function menuItemClick () {
-  mainMenu.style.display = 'none';
-  xIcon.style.display = 'none';
-  toggleButton.style.display = 'flex';
-  navbar.style.flexDirection = 'row';
+  mainMenu.style.display = 'none'
+  xIcon.style.display = 'none'
+  toggleButton.style.display = 'flex'
+  navbar.style.flexDirection = 'row'
 }
