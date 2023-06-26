21c2d77b059fb92b2b159c4fa93f03930d0971ae
Hossain <arafat.kd99@gmail.com>
2023-06-26 14:36:51 +0100

    Remove comment-out codes

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// .fa-solid
-// .fa-bars
-// .toggle-button
-// .menu
-// .x-icon
 let toggleButton = document.querySelector(".toggle-button");
 let xIcon = document.querySelector(".x-icon");
 let mainMenu = document.querySelector(".menu");
@@ -19,20 +14,12 @@ menuItem.forEach(item => item.addEventListener('click', menuItemClick));
 function show () {
   mainMenu.style.display = 'flex';
   mainMenu.style.color = '#fff';
-  // mainMenu.style.backgroundColor = '#6070ff';
-  // // mainMenu.style.backgroundSize = 'cover';
-  // mainMenu.style.width = '100%';
-  // mainMenu.style.height = '100%';
-  // mainMenu.style.marginTop = '10%';
   mainMenu.style.opacity = '.93';
-  // body.style.backgroundColor = '#6070ff';
-  // body.style.filter = "blur(2px)";
   toggleButton.style.display =  'none';
   xIcon.style.display = 'flex';
   xIcon.style.right = '0';
   navbar.style.display = 'flex';
   navbar.style.flexDirection = 'column';
-  // logo.style.display = 'flex';
   logo.style.alignSelf = 'flex-start'
   logo.style.paddingTop = '9%';
 }
